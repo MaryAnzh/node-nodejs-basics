9df66aa486d4bf5684286f313254806c3812329e
MaryAnzh <maryang@mail.ru>
2026-02-02 18:32:00 +0300

    feat: upgrade webP converter

---
+++ src/webPConverter/webPConverter.test.ts
@@ -0,0 +1,8 @@
+import { promises as fs } from 'fs';
+import sharp from 'sharp';
+import path from 'path';
+import * as C from '../constants.ts';
+
+describe('convert to webP', () => {
+
+ });
--- src/webPConverter/webPConverter.ts
+++ src/webPConverter/webPConverter.ts
@@ -2,31 +2,62 @@ import { promises as fs } from 'fs';
 import sharp from 'sharp';
 import path from 'path';
 import * as C from '../constants.ts';
+import { fileURLToPath } from 'url';
+
+const formats = [
+    'jpg', 'jpeg', 'png', 'tif', 'tiff', 'avif', 'svg', 'raw'
+];
+const filterFormats = (fileNames: string[]) =>
+    fileNames
+        .filter((name) => {
+            const format = name.split('.').at(-1);
+            return format === undefined
+                ? undefined
+                : formats.includes(format);
+        });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const checkOutputPath = (outputFolderPath: string, arr: string[], inputFileName: string, index = 0): string => {
+    const baseName = path.basename(inputFileName, path.extname(inputFileName));
+    const fileName = index === 0 ? `${baseName}.webp` : `${baseName}_${index}.webp`;
+    const p = path.join(outputFolderPath, fileName);
+
+    if (arr.includes(p)) {
+        return checkOutputPath(outputFolderPath, arr, inputFileName, index + 1);
+    }
+    return p;
+};
 
 export const convertImgToWebp = async (inputFolder: string, outputFolder: string) => {
     //read files from input dir
-    const inputFolderPath = path.resolve(inputFolder);
+    const makePath = (dir: string) => `../access/${dir}`;
+    const inputFolderPath = path.resolve(__dirname, makePath(inputFolder));
     let filesSet: string[] = [];
 
     try {
         await fs.access(inputFolderPath);
         const files = await fs.readdir(inputFolderPath);
-        filesSet = files;
+        filesSet = filterFormats(files);
     } catch {
         const errMessage = `${inputFolderPath} ${C.NOT_FOUND}`;
         throw new Error(errMessage);
     }
 
     if (filesSet.length === 0) {
-        console.log(`The ${inputFolder} folder is clean`);
-        return [];
+        console.log(`The ${inputFolder} folder hasn't file for convert`);
+        return filesSet;
     }
 
-    const outputFolderPath = path.resolve(outputFolder);
+    const outputFolderPath = path.resolve(__dirname, makePath(outputFolder));
 
     try {
         await fs.access(outputFolderPath);
-        throw new Error(`${C.FOLDER_EXIST}: ${outputFolderPath}`);
+        const content = await fs.readdir(outputFolderPath, { recursive: true });
+        if (content.length > 0) {
+            throw new Error(`${C.FOLDER_EXIST}: ${outputFolderPath}`);
+        }
     } catch (err) {
         if ((err as NodeJS.ErrnoException).code !== C.ENOENT) {
             throw err;
@@ -35,10 +66,10 @@ export const convertImgToWebp = async (inputFolder: string, outputFolder: string
 
     await fs.mkdir(outputFolderPath, { recursive: true });
     const results: string[] = [];
+
     for (const inputFileName of filesSet) {
         const inputPath = path.join(inputFolderPath, inputFileName);
-        const fileName = path.basename(inputFileName, path.extname(inputFileName)) + '.webp';
-        const outputPath = path.join(outputFolderPath, fileName);
+        const outputPath = checkOutputPath(outputFolderPath, results, inputFileName);
 
         try {
             await sharp(inputPath).webp({ quality: 80 })
